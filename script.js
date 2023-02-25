a94cd1351a46a7ea1587f200e9e8b6365492da33
AdrianoDEV79 <102677183+AdrianoDEV79@users.noreply.github.com>
2023-02-25 09:46:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ function calcular() {
     resultado.innerHTML += `<img style="max-width: 150px;" src="https://imagensemoldes.com.br/wp-content/uploads/2020/04/Ilustra%C3%A7%C3%A3o-Cerveja-PNG-1024x814.png">`
     resultado.innerHTML += `<p> ${Math.ceil(qtdtcerveja / 355)} Latas de Cerveja </p>`;
     resultado.innerHTML += `<img style="max-width: 100px;" src="./refrigerante-e-agua.png">`
-    resultado.innerHTML += `<p> ${Math.ceil(qtdtbebida / 2000)} Litros de Refrigerante </p>`;
+    resultado.innerHTML += `<p> ${Math.ceil(qtdtbebida / 2000)} Litros de Bebidas </p>`;
 
 }
 
